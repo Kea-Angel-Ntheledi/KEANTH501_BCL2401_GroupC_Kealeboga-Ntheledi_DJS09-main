e6cf8816153780a6c30d1788a6693449ad5ab9f9
Software Engineer 16 <Software.Engineer16@bafokeng.local>
2024-06-11 11:30:42 +0200

    Updated the code for the index.typescript file

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,21 @@
-// Literal Types
-// 1. Based on what we have learnt about literal types with the price, can you make
-// a Country literal type? You only have to include the countries we are dealing with in 
-// the project.
-// 2. Can you create a file and store all your types aliases in there?
-
-import { showReviewTotal, populateUser, showDetails } from './utils'
+import { showReviewTotal, populateUser, showDetails, getTopTwoReviews} from './utilis'
 import { Permissions , LoyaltyUser } from './enums'
-import { Price, Country } from './types'
+import { Review, Property } from './interface'
+import MainProperty from './classes' 
 const propertyContainer = document.querySelector('.properties')
-const footer = document.querySelector('.footer')
+const reviewContainer = document.querySelector('.reviews')
+const container = document.querySelector('.container')
+const button = document.querySelector('button')
+const footer = document.querySelector('.footer');
+
+import "./index.css"
 
 let isLoggedIn: boolean
 
 // Reviews
-const reviews : any[] = [
+const reviews: Review[] = [
     {
-        name: 'Sheia',
+        name: 'Sheila',
         stars: 5,
         loyaltyUser: LoyaltyUser.GOLD_USER,
         date: '01-04-2021'
@@ -31,7 +31,6 @@ const reviews : any[] = [
         stars: 4,
         loyaltyUser: LoyaltyUser.SILVER_USER,
         date: '27-03-2021',
-        description: 'Great hosts, location was a bit further than said.'
     },
 ]
 
@@ -45,19 +44,7 @@ const you = {
 }
 
 // Array of Properties
-const properties : {
-    image: string;
-    title: string;
-    price: Price;
-    location: {
-        firstLine: string;
-        city: string;
-        code: number;
-        country: Country;
-    };
-    contact: [ number, string ];
-    isAvailable: boolean;
-}[] = [
+const properties : Property[] = [
     {
         image: 'images/colombia-property.jpg',
         title: 'Colombian Shack',
@@ -72,7 +59,7 @@ const properties : {
         isAvailable: true  
     },
     {
-        image: 'images/poland-property.jpg',
+        image: '../images/poland-property.jpg',
         title: 'Polish Cottage',
         price: 30,
         location: {
@@ -85,17 +72,30 @@ const properties : {
         isAvailable: false 
     },
     {
-        image: 'images/london-property.jpg',
+        image: '../images/london-property.jpg',
         title: 'London Flat',
         price: 25,
         location: {
             firstLine: 'flat 15',
             city: 'London',
-            code: 35433,
+            code: 'SW4 5XW',
             country: 'United Kingdom',
         },
         contact: [+34829374892553, 'andyluger@aol.com'],
         isAvailable: true
+    },
+    {
+        image: '../images/malaysian-hotel.jpeg',
+        title: 'Malia Hotel',
+        price: 35,
+        location: {
+            firstLine: 'Room 4',
+            city: 'Malia',
+            code: 45334,
+            country: 'Malaysia'
+        },
+        contact: [ +60349822083, 'lee34@gmail.com'],
+        isAvailable: false
     }
 ]
 
@@ -116,6 +116,38 @@ for (let i = 0; i < properties.length; i++) {
     propertyContainer.appendChild(card)
 }
 
+let count = 0
+function addReviews(array : Review[]) : void {
+    if (!count ) {
+        count++
+        const topTwo = getTopTwoReviews(array)
+        for (let i = 0; i < topTwo.length; i++) {
+            const card = document.createElement('div')
+            card.classList.add('review-card')
+            card.innerHTML = topTwo[i].stars + ' stars from ' + topTwo[i].name
+            reviewContainer.appendChild(card)
+        }
+        container.removeChild(button) 
+    }
+}
+
+button.addEventListener('click', () => addReviews(reviews))
+
 let currentLocation : [string, string, number] = ['London', '11.03', 17]
 footer.innerHTML = currentLocation[0] + ' ' + currentLocation[1] + ' ' + currentLocation[2] + '°'
 
+
+let yourMainProperty = new MainProperty(
+    '../images/italian-property.jpg', 
+    'Italian House',
+    [{
+        name: 'Olive',
+        stars: 5,
+        loyaltyUser: LoyaltyUser.GOLD_USER,
+        date: '12-04-2021'
+    }] )
+
+const mainImageContainer = document.querySelector('.main-image')
+const image = document.createElement('img')
+image.setAttribute('src', yourMainProperty.src)
+mainImageContainer.appendChild(image)
